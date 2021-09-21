e2dcd65ad2c4a78042ee2984e12e089b652f1e4b
Steven Skeard <stevenskeard@gmail.com>
2021-09-20 22:10:07 -0400

    Implement cartridge slot

---
+++ src/nes/architecture/cartridge_slot.rs
@@ -0,0 +1,90 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// MIT License
+//
+// Copyright (c) 2021 fontivan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+use std::fs::File;
+use std::io::Read;
+
+pub struct CartridgeSlot {
+    file_path: String,
+    pub rom_contents: String,
+}
+
+impl CartridgeSlot {
+    // Constructor for CartidgeSlot
+    pub fn new(file_path: String) -> CartridgeSlot {
+
+        // Initialize an empty slot
+        let mut cartridge_slot: CartridgeSlot = CartridgeSlot {
+            file_path: "".to_string(),
+            rom_contents: "".to_string(),
+        };
+
+        // Load the file
+        cartridge_slot.load_cartridge(file_path);
+
+        // Return the cartridge slot
+        return cartridge_slot;
+    }
+
+    pub fn load_cartridge(&mut self, file_path: String) {
+
+        // If the path is empty then remove the current cartridge
+        if file_path == "" {
+            self.file_path = "".to_string();
+            self.rom_contents = "".to_string();
+            return;
+        }
+
+        // Set the path for the cartridge slot
+        self.file_path = file_path;
+
+        // Create the file using the provided path
+        let mut opened_file: File;
+
+        match File::open(self.file_path.clone()){
+            Ok(file) =>{
+                opened_file = file;
+            },
+            Err(_) => {
+                panic!("Unable to open file '{0}'.", self.file_path);
+            }
+        }
+
+        // Initialize the string that will store the file content
+        let mut file_content: String = String::new();
+
+        // Read the content from the file into the string
+        match opened_file.read_to_string(&mut file_content){
+            Ok(_) =>{
+                //no-op
+            }
+            Err(_) => {
+                panic!("Unable to read data from file '{0}'.", self.file_path)
+            }
+        }
+
+        // Set the self file content equal to the read data
+        self.rom_contents = file_content;
+    }
+}
--- src/nes/mod.rs
+++ src/nes/mod.rs
@@ -22,18 +22,20 @@
 // SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+use crate::nes::architecture::cartridge_slot::CartridgeSlot;
+use crate::nes::architecture::cpu::Cpu;
+use crate::nes::architecture::memory::Memory;
+
 pub mod architecture;
 pub mod controllers;
 pub mod instructions;
 pub mod mappers;
 
-use crate::nes::architecture::cpu::Cpu;
-use crate::nes::architecture::memory::Memory;
-
 pub struct Nes {
+    cartridge_slot: CartridgeSlot,
     cpu: Cpu,
     // apu: Apu,
-    memory: Memory
+    memory: Memory,
     // ppu: Ppu,
     // controllers: Controllers
 }
@@ -43,15 +45,15 @@ impl Nes {
     pub fn new() -> Self {
 
 
-        // Initialize the memory with 8KB
-        let memory_size: usize = 8192000;
-        let memory_result:Result<Memory, usize> = Memory::new(memory_size);
+        // Initialize the memory with 2KB (RAM) + 1MB (ROM)
+        let memory_size: usize = 1024*1000*2 + 1024*1000*1000;
+        let memory_result: Result<Memory, usize> = Memory::new(memory_size);
         let memory: Memory;
         match memory_result {
             Ok(result) => {
                 memory = result;
             }
-            Err(_) =>{
+            Err(_) => {
                 panic!("Unable to initialize memory");
             }
         }
@@ -59,16 +61,20 @@ impl Nes {
         // Initialize the CPU
         let cpu: Cpu = Cpu::new();
 
+        // Initialize the cartridge slot
+        let cartridge_slot: CartridgeSlot = CartridgeSlot::new("".to_string());
+
         // Construct the Nes
         return Nes {
+            cartridge_slot,
             cpu,
-            memory
-        }
+            memory,
+        };
     }
 
-    fn dump_memory(&mut self){
+    fn dump_memory(&mut self) {
         let memory_size = self.memory.get_size();
-        for i in 0..memory_size{
+        for i in 0..memory_size {
             self.memory.write(i, [1].to_vec());
         }
         print!("{:?}", self.memory.read(0, memory_size));
