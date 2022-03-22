6e362d13785ef41e3272df5ea25e4873e63d985d
Steven Skeard <stevenskeard@gmail.com>
2022-03-22 15:12:21 -0400

    Fix some syntax issues

--- src/nes/instructions/legal/opcode0x2a.rs
+++ src/nes/instructions/legal/opcode0x2a.rs
@@ -37,37 +37,37 @@ impl Opcode for Opcode0x2a {
         // Rotate left on the accumulator register
 
         // Get the value from the accumulator
-        value = _cpu.accumulator
+        let value = _cpu.accumulator;
 
         // Get the high bit from the value
-        high_bit = value & 0b1000_0000
+        let high_bit = value & 0b1000_0000;
 
         // Shift the value
-        value = value << 1
+        value = value << 1;
 
         // Set the lowest bit to be the value from the carry flag
         if _cpu.carry == 1 {
-            value = value | 0b0000_0001
+            value = value | 0b0000_0001;
         }
 
         // Save the value back to the accumulator
-        _cpu.accumulator = value
+        _cpu.accumulator = value;
 
         // Save the high bit into the carry
-        _cpu.carry = high_bit
+        _cpu.carry = high_bit;
 
         // Conditionally set the zero flag
         if value == 0 {
-            _cpu.set_z_flag()
+            _cpu.set_z_flag();
         } else {
-            _cpu.clear_z_flag()
+            _cpu.clear_z_flag();
         }
         
         // Conditionally set the negative flag
         if value & 0b1000_0000 == 0b1000_0000 {
-            _cpu.set_n_flag()
+            _cpu.set_n_flag();
         } else {
-            _cpu.clear_n_flag()
+            _cpu.clear_n_flag();
         }
 
     }
