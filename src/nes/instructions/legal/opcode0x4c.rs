f712b449a456e909e8cc40c401e9ba6c77955aa0
Steven Skeard <stevenskeard@gmail.com>
2022-07-27 11:39:32 -0230

    Add unit tests for jump instruction 0x4c

--- src/nes/instructions/legal/opcode0x4c.rs
+++ src/nes/instructions/legal/opcode0x4c.rs
@@ -39,3 +39,38 @@ impl Opcode for Opcode0x4c {
         _cpu.program_counter = address;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_cpu() -> Cpu {
+        // Get a cpu
+        let mut cpu: Cpu = Cpu::new();
+        cpu.program_counter = 0x00;
+        return cpu;
+    }
+
+    fn get_test_memory() -> Memory {
+        // Get a memory
+        let memory_size: usize = 16;
+        let memory_result: Result<Memory, usize> = Memory::new(memory_size);
+        let mut memory: Memory = memory_result.unwrap();
+        return memory;
+    }
+
+    #[test]
+    fn test_execute() {
+        // Prep for the test
+        let mut cpu: Cpu = get_test_cpu();
+        let mut memory: Memory = get_test_memory();
+        memory.write(0, [0x4c, 0x12, 0x34].to_vec());
+
+        // Execute instruction
+        cpu.program_counter = 0x01;
+        Opcode0x4c::execute(&mut cpu, &mut memory);
+
+        // Assert results
+        assert_eq!(cpu.program_counter, 0x3412);
+    }
+}
